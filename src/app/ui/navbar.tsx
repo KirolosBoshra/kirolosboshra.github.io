c3b281b05b4810ee72c5153321bcd83f52c59e87
kingkiro99 <kingkiro8@gmail.com>
2024-10-21 16:22:25 +0300

    Remove Search Bar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,27 +1,19 @@
 import Link from "next/link";
-import { SearchBar } from "./searchBar";
 import { ThemeSwitcher } from "./themeSwitcher";
 
-type prop = {
-  title: string
-};
-
-export function NavBar(prop: prop) {
+export function NavBar({ title }: { title: String }) {
   return (
-    <div className="top-0 flex flex-row items-center justify-between px-12 max-sm:px-6 space-x-4 w-full h-20 border-b 
+    <div className="top-0 flex flex-row items-center justify-between px-12 max-sm:px-6 w-full h-20 border-b 
         border-b-black dark:border-b-zinc-500 dark:bg-black bg-opacity-80 backdrop-blur-sm z-10">
-      <div className="flex justify-center space-x-4">
+      <div className="flex space-x-4">
         <Link href="/" className="text-2xl"><span>▼</span></Link>
         <Link href="/" className="text-xl max-sm:text-lg">Portfolio</Link>
         <Link href="/" className="text-xl max-sm:text-lg">Contact</Link>
       </div>
-      <div>
-        <span className="text-xl max-sm:hidden">{prop.title}</span>
+      <div className="flex mr-40 justify-center items-center">
+        <span className="text-xl max-sm:hidden">{title}</span>
       </div>
-      <div className="flex flex-row items-center space-x-4">
-        <SearchBar />
-        <ThemeSwitcher />
-      </div>
+      <ThemeSwitcher />
     </div>
   );
 }
