799c6486d21e883e7493399b4f13b0095cd3859b
kingkiro99 <kingkiro8@gmail.com>
2024-10-22 19:35:01 +0300

    'Deploy'

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Ensures that next export works
-  basePath: '/', // Set the base path to match your GitHub repo name
+  basePath: '', // Set the base path to match your GitHub repo name
   assetPrefix: '/', // Set asset prefix for static files
 };
 
